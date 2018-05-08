fc50596def72af85e7e930f5a7379ccf1de4c649
alec <ajcuccia@gmail.com>
2018-05-08 14:40:14 -0400

    Add session actions, session ajax requests, root and app components, and the store

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,64 @@
+import * as SessionApiUtils from '../util/session_api_utils';
+
+// session action types
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+// synchronous session action creators
+export const receiveCurrentUser = (currentUser) => {
+  return (
+    {
+      type: RECEIVE_CURRENT_USER,
+      currentUser
+    }
+  );
+};
+
+export const logoutCurrentUser = () => {
+  return (
+    {
+      type: LOGOUT_CURRENT_USER
+    }
+  );
+};
+
+export const receiveSessionErrors = (errors) => {
+  return (
+    {
+      type: RECEIVE_SESSION_ERRORS,
+      errors
+    }
+  );
+};
+
+// thunk session action creators
+export const signup = (user) => (dispatch) => {
+  return SessionApiUtils.signup(user).then(
+    (user) => {
+      return dispatch(receiveCurrentUser(user));
+    },
+    (errors) => {
+      return dispatch(receiveSessionErrors(errors.responseJSON));
+    }
+  );
+};
+
+export const login = (user) => (dispatch) => {
+  return SessionApiUtils.login(user).then(
+    (user) => {
+      return dispatch(receiveCurrentUser(user));
+    },
+    (errors) => {
+      return dispatch(receiveSessionErrors(errors.responseJSON));
+    }
+  );
+};
+
+export const logout = () => (dispatch) => {
+  return SessionApiUtils.logout().then(
+    (user) => {
+      return dispatch(logoutCurrentUser());
+    }
+  );
+};
---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>peanut butter and enjambment sandwich</h1>
+      </header>
+    </div>
+  )
+};
+
+export default App;
