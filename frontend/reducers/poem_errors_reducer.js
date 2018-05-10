5e35a52699f8a25549c83b3b69df8d5ad253fee4
alec <ajcuccia@gmail.com>
2018-05-10 14:38:14 -0400

    Fix bug in PoemErrorsReducer

--- frontend/reducers/poem_errors_reducer.js
+++ frontend/reducers/poem_errors_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_POEM_ERRORS } from '../actions/poem_actions';
 
-const PoemErrorsReducer = (previousState = {}, action) => {
+const PoemErrorsReducer = (previousState = [], action) => {
   Object.freeze(previousState);
   switch (action.type) {
     case RECEIVE_POEM_ERRORS:
