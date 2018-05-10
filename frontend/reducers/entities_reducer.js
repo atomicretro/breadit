fe6390ece5d3de805e2f372614fd40989715ea78
alec <ajcuccia@gmail.com>
2018-05-10 14:44:52 -0400

    Add PoemsReducer to EntitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
+import PoemsReducer from './poems_reducer';
 
 const EntitiesReducer = combineReducers({
-  users: UsersReducer
+  users: UsersReducer,
+  poems: PoemsReducer
 });
 
 export default EntitiesReducer;
