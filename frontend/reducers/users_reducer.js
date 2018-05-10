6bbc151e9d35854dc218ed4db4525551f4b14bf7
alec <ajcuccia@gmail.com>
2018-05-10 14:36:33 -0400

    Have UsersReducer listen for LOGOUT action and clear user slice accordingly

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,11 +1,20 @@
 import { merge } from 'lodash';
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
 
 const UsersReducer = (previousState = {}, action) => {
   Object.freeze(previousState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, previousState, { [action.currentUser.id]: action.currentUser} );
+      return merge(
+        {},
+        previousState,
+        { [action.currentUser.id]: action.currentUser }
+      );
+    case LOGOUT_CURRENT_USER:
+      return {};
     default:
       return previousState;
   }
