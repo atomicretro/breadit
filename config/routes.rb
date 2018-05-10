926afc761f75e33465d2e3c5219e153847658c4e
Ryan <rnowusu@gwu.edu>
2018-05-10 10:57:43 -0400

    Create Poem and Author routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:show, :create, :destroy]
+    resources :poems, only: [:index, :show, :create, :update]
+    resources :author, only: [:create]
   end
 end
