054ce0f79f4485c58d6dea9303fd3b05bbb6366d
Ryan <rnowusu@gwu.edu>
2018-05-10 12:18:25 -0400

    Create Poem API utils

---
+++ frontend/util/poem_api_utils.js
@@ -0,0 +1,38 @@
+export const fetchPoems = (poems) => {
+  return $.ajax({
+    method: 'GET',
+    url: '/api/poems',
+    data: { poems }
+  });
+};
+
+export const fetchPoem = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/poems/${id}`,
+    data: { id }
+  });
+};
+
+export const createPoem = (poem) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/poems',
+    data: { poem }
+  });
+};
+
+export const updatePoem = (poem) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/poems/${poem.id}`,
+    data: { poem }
+  });
+};
+
+export const deletePoem = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/poems/${id}`,
+  });
+};
