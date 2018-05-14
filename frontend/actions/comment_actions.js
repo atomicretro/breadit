51266e62755d34d0fac08216d908e74c197b1c4e
alec <ajcuccia@gmail.com>
2018-05-14 17:31:54 -0400

    Add receieveCommentErrors action

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -2,6 +2,7 @@ import * as CommentApiUtils from '../util/comment_api_utils';
 
 // comment action types
 export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const RECEIVE_COMMENT_ERRORS = 'RECEIVE_COMMENT_ERRORS';
 
 // synchronous comment action creators
 export const receiveComment = ({ comment }) => {
@@ -9,6 +10,13 @@ export const receiveComment = ({ comment }) => {
     type: RECEIVE_COMMENT,
     comment
   };
+};
+
+export const receiveCommentErrors = (errors) => {
+  return ({
+    type: RECEIVE_COMMENT_ERRORS,
+    errors
+  });
 };
 
 // thunk comment action creators
