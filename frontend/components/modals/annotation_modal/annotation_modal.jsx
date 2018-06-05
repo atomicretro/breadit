a0c8a21e104bafab02b1b920b6b326749011c240
alec cuccia <ajcuccia@gmail.com>
2018-06-05 17:13:51 -0400

    Refactor annotation modal to be a purely functional component

--- frontend/components/modals/annotation_modal/annotation_modal.jsx
+++ frontend/components/modals/annotation_modal/annotation_modal.jsx
@@ -4,46 +4,38 @@ import { Route, Link, Switch } from 'react-router-dom';
 import AnnotationFormContainer from '../../annotations/annotation_forms/annotation_form_container';
 import AnnotationItemContainer from '../../annotations/annotation_items/annotation_item_container';
 
-class AnnotationModal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const AnnotationModal = (ownProps) => {
+  if(!ownProps.modal) return null;
 
-  render() {
-    if (!this.props.modal) {
-      return null;
-    }
+  let poemText = document
+    .getElementsByClassName('poem-text')[0] || { clientHeight: 0 };
+  let poemHeight = poemText.clientHeight;
 
-    let poemText = document
-      .getElementsByClassName('poem-text')[0] || { clientHeight: 0 };
-    let poemHeight = poemText.clientHeight;
-
-    return (
+  return (
+    <div
+      className="annotation-modal"
+      onClick={ (e) => e.stopPropagation() }
+      data="annotation" >
       <div
-        className="annotation-modal"
-        onClick={ (e) => e.stopPropagation() }
+        style={ {minHeight: poemHeight + 39} }
+        className="annotation-container"
         data="annotation" >
         <div
-          style={ {minHeight: poemHeight + 39} }
-          className="annotation-container"
+          className="annotation-title"
           data="annotation" >
-          <div
-            className="annotation-title"
-            data="annotation" >
-            <h3 data="annotation">enjambment annotation</h3>
-          </div>
-          <Switch>
-            <Route
-              exact path="/poems/:poemId/annotations/:annotationId"
-              component={AnnotationItemContainer} />
-            <Route
-              exact path="/poems/:poemId"
-              component={AnnotationFormContainer} />
-          </Switch>
+          <h3 data="annotation">enjambment annotation</h3>
         </div>
+        <Switch>
+          <Route
+            exact path="/poems/:poemId/annotations/:annotationId"
+            component={AnnotationItemContainer} />
+          <Route
+            exact path="/poems/:poemId"
+            component={AnnotationFormContainer} />
+        </Switch>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withRouter(AnnotationModal);
