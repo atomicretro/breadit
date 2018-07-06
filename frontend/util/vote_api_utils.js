55848ab6da7e0906d710aa5e4d6e6040aa631a17
alec cuccia <ajcuccia@gmail.com>
2018-07-06 15:18:54 -0400

    Refactor vote API utils to work with objects and accept both voting directions

--- frontend/util/vote_api_utils.js
+++ frontend/util/vote_api_utils.js
@@ -1,14 +1,14 @@
-export const createPoemUpvote = ({ type, type_id, vote_direction }) => {
+export const createPoemUpvote = ({ type, type_id, direction }) => {
   return $.ajax({
     method: 'POST',
-    url: `/api/${type}/${type_id}/upvotes`,
+    url: `/api/${type}/${type_id}/${direction}s`,
     data: { vote_direction }
   });
 };
 
-export const deletePoemUpvote = (type, type_id, vote_id) => {
+export const deletePoemUpvote = ({ type, type_id, direction, vote_id }) => {
   return $.ajax({
     method: 'DELETE',
-    url: `/api/${type}/${type_id}/upvotes/${vote_id}`
+    url: `/api/${type}/${type_id}/${direction}s/${vote_id}`
   });
 };
